ae54e6a2e5774c61ea57ce08b0485de156d7ba18
AquaDevelopers - Chuck Konkol <myaquaapps@gmail.com>
2024-10-25 09:46:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ async function loadTips() {
     console.log(doc.data().url);
     if ((doc.data().url == null) || (doc.data().url == ""))
     {
- tipElement.innerHTML = "<h3>" + doc.data().name + "</h3><p>" + doc.data().content + "</p>";
+ tipElement.innerHTML = "<h3>" + doc.data().name + "</h3><p>" + doc.data().content + "</p><p>" + doc.data().timestamp + "</p>";
   }else{
- tipElement.innerHTML = "<h3>" + doc.data().name + "</h3><p>" + doc.data().content + "</p><p><a href='" + doc.data().url + "' target='_blank'>link</a></p>";
+ tipElement.innerHTML = "<h3>" + doc.data().name + "</h3><p>" + doc.data().content + "</p><p><a href='" + doc.data().url + "' target='_blank'>link</a></p><p>" + doc.data().timestamp + "</p>";
   }
        tipsContainer.appendChild(tipElement);
   });
