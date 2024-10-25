dbb6289326108b902e5593cc98aa9fc997987536
AquaDevelopers - Chuck Konkol <myaquaapps@gmail.com>
2024-10-25 12:48:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,3 +67,22 @@ function formatDate(date) {
     );
     return formatDate.toLocaleTimeString('en-us', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 }
+
+// Fetch all the details element.
+const details = document.querySelectorAll("details");
+
+// Add the onclick listeners.
+details.forEach((detail) => {
+  detail.addEventListener("toggle", () => {
+    if (detail.open) setTargetDetail(detail);
+  });
+});
+
+// Close all the details that are not targetDetail.
+function setTargetDetail(targetDetail) {
+  details.forEach((detail) => {
+    if (detail !== targetDetail) {
+      detail.open = false;
+    }
+  });
+}
