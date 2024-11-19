186b49f025a9a46c07bb7d91b491e77a72a70128
AquaDevelopers - Chuck Konkol <myaquaapps@gmail.com>
2024-11-19 06:20:39 -0600

    Update getusername.php

--- getusername.php
+++ getusername.php
@@ -1,3 +1,21 @@
-<php>
-  echo gethostname();
-</php>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+echo gethostname();
+?>
+
+</body>
+</html>
